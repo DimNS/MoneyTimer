e8470a0855304cb6453bdcfb5b8e513a0f759dfd
Дмитрий Щербаков <atomcms@ya.ru>
2019-05-28 11:19:41 +0500

    fix Функция отображения ошибки из приложения

--- src/js/0-app/100-showError.js
+++ src/js/0-app/100-showError.js
@@ -1,13 +1,22 @@
 /**
- * Инициализация приложения
+ * Функция отображения ошибки из приложения
  *
  * @param {{status: string, code: string, title: string}} error
  *
  * @type {function}
  *
- * @version 19.05.2019
+ * @version 28.05.2019
  * @author  Дмитрий Щербаков <atomcms@ya.ru>
  */
 app.showError = function (error) {
-    alert('status: ' + error.status + '\ncode: ' + error.code + '\ntitle: ' + error.title);
+    var modal = $('#js-modal__alert');
+
+    modal.find('.modal-header').removeClassWild('bg-*').addClass('bg-' + error.code);
+    modal.find('.modal-title').html(error.status);
+    modal.find('.modal-body').html(error.title);
+
+    modal.modal({
+        backdrop: 'static',
+        keyboard: false,
+    });
 };
